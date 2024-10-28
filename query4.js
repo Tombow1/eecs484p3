fa417cf89ae6623475f778b47088aed79c0f19bd
ziyaoyan <125417595+Tombow1@users.noreply.github.com>
2024-10-28 01:25:10 -0400

    Update query4.js

--- query4.js
+++ query4.js
@@ -18,7 +18,18 @@ function suggest_friends(year_diff, dbname) {
     db = db.getSiblingDB(dbname);
 
     let pairs = [];
-    // TODO: implement suggest friends
-
+    db.users.find().forEach(userA => {
+        db.users.find().forEach(userB => {
+            if (userA.gender === 'male' && userB.gender === 'female') {
+                if (Math.abs(userA.YOB - userB.YOB) < year_diff) {
+                    if (userA.hometown.city === userB.hometown.city) {
+                        if (!userA.friends.includes(userB.user_id) && !userB.friends.includes(userA.user_id)) {
+                            pairs.push([userA.user_id, userB.user_id]);
+                        }
+                    }
+                }
+            }
+        });
+    });
     return pairs;
 }
