d0262b0d6c03153a277be73210c3a3fbf4a96cc9
ziyaoyan <125417595+Tombow1@users.noreply.github.com>
2024-10-29 02:21:16 -0400

    Update query5.js

--- query5.js
+++ query5.js
@@ -12,6 +12,50 @@ function oldest_friend(dbname) {
 
     let results = {};
     // TODO: implement oldest friends
+    db.users.aggregate([
+        {
+            $project: {
+                user_id: 1,
+                friends: 1,
+                _id: 0
+            }
+        },
+        {
+            $unwind: "$friends"
+        },
+        {
+            $out: "flat_users"
+        }
+    ]);
 
+
+    let symmetricFriends = {};
+    db.flat_users.find().forEach(user => {
+        let user_id = user.user_id;
+        let friend_id = user.friends;
+
+        if (!symmetricFriends[user_id]) {
+            symmetricFriends[user_id] = [];
+        }
+        if (!symmetricFriends[friend_id]) {
+            symmetricFriends[friend_id] = [];
+        }
+
+        if (!symmetricFriends[user_id].includes(friend_id)) {
+            symmetricFriends[user_id].push(friend_id);
+        }
+        if (!symmetricFriends[friend_id].includes(user_id)) {
+            symmetricFriends[friend_id].push(user_id);
+        }
+    });
+
+    for (let user_id in symmetricFriends) {
+        let friends = Array.from(symmetricFriends[user_id]);
+        if (friends.length > 0) {
+            let oldest = db.users.find({ user_id: { $in: friends } },{ user_id: 1, YOB: 1, _id: 0 }).sort({ YOB: 1, user_id: 1 }).toArray();
+            results[user_id] = oldest[0].user_id;
+        }
+
+    }
     return results;
 }
